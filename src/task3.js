f437729f945fa427fa2f643677c197934d70fec3
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-04-13 16:04:16 +1200

    Completed task 3.

---
+++ src/task3.js
@@ -0,0 +1,18 @@
+// Declared variables with numerical values.
+
+let likes = 10
+let dislikes = 5
+
+// Declared a function that passes the variables as parameters then subtracts one from the other.
+
+function calcLikes(likes, dislikes) {
+    return likes - dislikes
+}
+
+// A variable that returns the function as an answer.
+
+let answer = calcLikes(likes, dislikes)
+
+// Printing the result.
+
+console.log(answer)
