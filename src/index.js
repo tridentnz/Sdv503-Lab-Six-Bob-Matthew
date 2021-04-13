d2307dc0625762953875e788c1ee36b108184748
MattyHucks <matthew-hucker@live.nmit.ac.nz>
2021-04-13 15:40:56 +1200

    Recommit (Code not found it original commit.)

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//TODO: Add the odd numbers within an array. Handle invalid input.
+
+function isNumber(arr) { // Checks array for invalid data types
+	for(let i = 0; i < arr.length; i++) {
+		if (isNaN(arr[i])) return false;
+	}
+	return true;
+}
+
+function isOdd(arr) { // Returns false if the array contains any strings.
+	for(let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			return false;
+		}
+	return true;	
+	}
+}
+
+function addOdd(arr) {	// Adds values within the array
+	return arr.reduce(reducer);
+}
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+let input = [1, 3, 5, 'a'];
+if (isNumber(input) && isOdd(input)) { // If data validation functions return true, invoke 'addOdd' function which adds the values contained in 'input' array.
+	console.log(addOdd(input));
+}
+else console.log("Please input odd numbers.");
