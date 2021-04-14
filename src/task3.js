417ec302934749c15b48459cd325d1c4f04a7a55
MattyHucks <matthew-hucker@live.nmit.ac.nz>
2021-04-15 08:26:09 +1200

    Created a single object containing likes and dislikes.

--- src/task3.js
+++ src/task3.js
@@ -1,7 +1,9 @@
 // Declared variables with numerical values.
 
-let likes = 10
-let dislikes = 5
+let rating = {
+    likes: 10,
+    dislikes: 5
+}
 
 // Declared a function that passes the variables as parameters then subtracts one from the other.
 
@@ -11,7 +13,7 @@ function calcLikes(likes, dislikes) {
 
 // A variable that returns the function as an answer.
 
-let answer = calcLikes(likes, dislikes)
+let answer = calcLikes(rating.likes, rating.dislikes)
 
 // Printing the result.
 
